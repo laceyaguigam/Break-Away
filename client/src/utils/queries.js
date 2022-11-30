09452ea64fcac6ab610e928f5637077b59ee635b
G Codes <graciela.mejia1@yahoo.com>
2022-11-29 22:16:20 -0800

    uncommented client/utils auth.js, mutations.js, queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,12 @@
-// import { gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-// export const GET_ME = gql`
-//   {
-//     me {
-//       _id
-//       username
-//       email
-//       }
-//     }
-//   }
-// `;
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      }
+    }
+  }
+`;
