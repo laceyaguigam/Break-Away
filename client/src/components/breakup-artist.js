19be74879d189006f76f9ffd0ff3b55f8bc0358f
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-30 15:37:56 -0800

    added new feature branch and added import/export initial code to breakup-artist

---
+++ client/src/components/breakup-artist.js
@@ -0,0 +1,17 @@
+import react from "react"
+import img1 from ''
+import img2 from ''
+import img3 from ''
+import img4 from ''
+import img5 from ''
+import img6 from ''
+
+
+
+function breakupArtist (){
+    return (
+
+
+    )
+}
+export default breakupArtist;
