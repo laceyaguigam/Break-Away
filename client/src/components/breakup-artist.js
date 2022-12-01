776215f1f279222525f1cf3f924dd93c232bb6d8
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-30 19:27:37 -0800

    added the basic code for the breakup artist profile cards

--- client/src/components/breakup-artist.js
+++ client/src/components/breakup-artist.js
@@ -10,13 +10,55 @@ import img6 from ''
 
 function breakupArtist (){
     return (
-       <div class="card">
-            <img src={img1} alt=""/>
-            <h1>(insert name)</h1>
-            <p class=" ">(insert text)</p>
-            <p> (more text)</p>
-            <p> <button>(select or pay here)</button></p>
-       </div>
+       <div>
+  <div className="card">
+    <img src="{img1}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+
+  <div className="card">
+    <img src="{img2}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+
+  <div className="card">
+    <img src="{img3}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+
+  <div className="card">
+    <img src="{img4}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+
+  <div className="card">
+    <img src="{img5}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+
+  <div className="card">
+    <img src="{img6}" alt />
+    <h1>(insert name)</h1>
+    <p className=" ">(insert text)</p>
+    <p> (more text)</p>
+    <p> <button>(select or pay here)</button></p>
+  </div>
+</div>
 
     )
 }
