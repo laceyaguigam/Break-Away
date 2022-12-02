10b0012586da9f74d26c11d17182eaca51a7ef7a
alexuschristensen <alexusdc6@gmail.com>
2022-12-01 20:33:46 -0800

    added refractor components for global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,50 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Navbar";
+import { StoreProvider } from "./utils/GlobalState";
 import Footer from "./components/Footer";
+import { ApolloProvider } from "@apollo/client";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* <header className="App-header"> */}
-        <Nav />
-        {/* </header> */}
-        <div>
-          <Routes></Routes>
-        </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          {/* <header className="App-header"> */}
+          <StoreProvider>
+            <Nav />
+            {/* </header> */}
+              <Routes>
+              <Route 
+                  path="/" 
+                element={<Home />} 
+                />
+                <Route 
+                  path="/login" 
+                element={<Login />}
+                />
+                <Route 
+                  path="/signup" 
+                element={<Signup />} 
+                />
+                <Route 
+                  path="/orderHistory" 
+                element={<OrderHistory />} 
+                />
+                <Route 
+                  path="/products/:id" 
+                element={<Detail />}
+                />
+                <Route 
+                  path="*" 
+                element={<NoMatch />}
+                />
+              </Routes>
+            </StoreProvider>
+          </div>
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
