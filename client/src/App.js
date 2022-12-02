0336d7e34674cd4526d3af125903f70e4cdf1abe
G Codes <graciela.mejia1@yahoo.com>
2022-12-01 23:37:59 -0800

    finally seeing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <header className="App-header"> */}
         <Nav />
-        {/* </header> */}
-        <div>
-          <Routes></Routes>
-        </div>
+        <div></div>
         <Footer />
       </div>
     </Router>
