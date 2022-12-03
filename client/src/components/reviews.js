bafcec4663de0664eecfe4f43c87d9a9e2f92ff6
catherinecunningham <cfunk512@gmail.com>
2022-12-03 13:57:21 -0800

    set up basic format for reviews component

---
+++ client/src/components/reviews.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Reviews() {
+    return (
+        <section>
+            {/* input reviews from Leave-a-Review component here */}
+        </section>
+    )
+}
+export default Reviews
