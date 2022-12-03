44f672d58de5cf548870bfaf7cc8e63e27730593
catherinecunningham <cfunk512@gmail.com>
2022-12-03 13:48:59 -0800

    set up leave a review form star rating

--- client/src/components/leave-a-review.js
+++ client/src/components/leave-a-review.js
@@ -7,8 +7,17 @@ function LeaveAReview() {
             <input type="text"></input><br></br>
             <label className="form-label">Breakup Artist Name:</label><br></br>
             <input type="text"></input><br></br>
+            <label className="form-label">Rating:</label>
+            <select name="rating" id="rating">
+                <option value="★">★</option>
+                <option value="★★">★★</option>
+                <option value="★★★">★★★</option>
+                <option value="★★★★">★★★★</option>
+                <option value="★★★★★">★★★★★</option>
+            </select>
             <label classname="form-label">Review:</label><br></br>
             <input type="textarea"></input>
         </form>
     )
 }
+export default LeaveAReview
