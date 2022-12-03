084f0f745186b33599323634052b2d0a9e5cdd26
alexuschristensen <alexusdc6@gmail.com>
2022-12-03 15:35:10 -0800

    saving to merge with develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, /*Route,*/} from "react-router-dom";
 import Nav from "./components/Navbar";
-import { StoreProvider } from "./utils/GlobalState";
-import Footer from "./components/Footer";
-import { ApolloProvider } from "@apollo/client";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          {/* <header className="App-header"> */}
-          <StoreProvider>
-            <Nav />
-            {/* </header> */}
-              <Routes>
-              <Route 
-                  path="/" 
-                element={<Home />} 
-                />
-                <Route 
-                  path="/login" 
-                element={<Login />}
-                />
-                <Route 
-                  path="/signup" 
-                element={<Signup />} 
-                />
-                <Route 
-                  path="/orderHistory" 
-                element={<OrderHistory />} 
-                />
-                <Route 
-                  path="/products/:id" 
-                element={<Detail />}
-                />
-                <Route 
-                  path="*" 
-                element={<NoMatch />}
-                />
-              </Routes>
-            </StoreProvider>
-          </div>
-        <Footer />
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <div className="App">
+        {/* <header className="App-header"> */}
+        <Nav />
+        {/* </header> */}
+        <div>
+          {/* <Routes></Routes> */}
+        </div>
+      {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
