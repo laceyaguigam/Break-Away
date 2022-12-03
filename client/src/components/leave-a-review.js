28469c2a85356c9c554a0d9a250a846c95e7607e
catherinecunningham <cfunk512@gmail.com>
2022-12-03 13:34:44 -0800

    create form for leave a review component

---
+++ client/src/components/leave-a-review.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LeaveAReview() {
+    return (
+        <form>
+            <label className="form-label">First Name:</label><br></br>
+            <input type="text"></input><br></br>
+            <label className="form-label">Breakup Artist Name:</label><br></br>
+            <input type="text"></input><br></br>
+            <label classname="form-label">Review:</label><br></br>
+            <input type="textarea"></input>
+        </form>
+    )
+}
