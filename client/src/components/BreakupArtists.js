b2c67ec842ce53fc2e8ecd864006a3309be81747
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-03 19:22:53 -0800

    commit to pull

--- client/src/components/BreakupArtists.js
+++ client/src/components/BreakupArtists.js
@@ -16,7 +16,7 @@ function BreakupArtists() {
   
   <div className="card" class="row">
     <div class="column">
-      <img src={img1} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+      <img src={img1} alt="photograph of Sarah the breakup artist" style={{width: '50%'}} />
       <h1>Alex</h1>
       <p className="title">(insert info)</p>
       <p>(insert info)</p>
@@ -26,7 +26,7 @@ function BreakupArtists() {
 
 <div className="card">
   <div class="column">
-    <img src={img2} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+    <img src={img2} alt="photograph of Sarah the breakup artist" style={{width: '260%'}} />
     <h1>Alicia</h1>
     <p className="title">(insert info)</p>
     <p>(insert info)</p>
@@ -34,9 +34,10 @@ function BreakupArtists() {
 </div>
 </div>
 
+
 <div className="card">
   <div class="column">
-    <img src={img3} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+    <img src={img3} alt="photograph of Sarah the breakup artist" style={{width: '260%'}} />
     <h1>Cassie</h1>
     <p className="title">(insert info)</p>
     <p>(insert info)</p>
@@ -44,28 +45,36 @@ function BreakupArtists() {
 </div>
 </div>
 
+<br></br>
+
 <div className="card" class="row">
-  <img src={img4} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+<div class="column">
+  <img src={img4} alt="photograph of Sarah the breakup artist" style={{width: '260%'}} />
   <h1>Joe</h1>
   <p className="title">(insert info)</p>
   <p>(insert info)</p>
   <p><button>Hire</button></p>
 </div>
+</div>
 
 <div className="card">
-  <img src={img5} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+  <div class="column">
+  <img src={img5} alt="photograph of Sarah the breakup artist" style={{width: '260%'}} />
   <h1>Kyle</h1>
   <p className="title">(insert info)</p>
   <p>(insert info)</p>
   <p><button>Hire</button></p>
 </div>
+</div>
 
 <div className="card">
-  <img src={img6} alt="photograph of Sarah the breakup artist" style={{width: '20%'}} />
+<div class="column">
+  <img src={img6} alt="photograph of Sarah the breakup artist" style={{width: '260%'}} />
   <h1>Sarah</h1>
   <p className="title">(insert info)</p>
   <p>(insert info)</p>
   <p><button>Hire</button></p>
+  </div>
 </div>
 </div>
 </section>
