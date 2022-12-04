320546124194b29bff721ccb74c890a6dd51626c
G Codes <graciela.mejia1@yahoo.com>
2022-12-03 16:42:54 -0800

    PAGE NOW RENDERS/placeholders for homepage and reviews added

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function HomePage() {
-  return <section>{/* home page stuff will go here */}</section>;
+  return (
+    <section>
+      {/* home page stuff will go here */}
+      <p>This is the homepage.</p>
+    </section>
+  );
 }
 export default HomePage;
--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Reviews() {
   return (
-    <section>{/* input reviews from Leave-a-Review component here */}</section>
+    <section>
+      {/* input reviews from Leave-a-Review component here */}
+      <p>this section is for reviews.</p>
+    </section>
   );
 }
 export default Reviews;
