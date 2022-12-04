da8836c66ed67bada9c83aedb7dd9dfd395728b2
alexuschristensen <alexusdc6@gmail.com>
2022-12-03 22:42:09 -0800

    created GraphQL query to generate a stripe session

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,23 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-const [state, dispatch] = useProductReducer({
-  products: [],
-  cart: [],
-  cartOpen: false,
-  categories: [],
-  currentCategory: ''
-});
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-  };
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+  });
 
-  const useStoreContext = () => {
-    return useContext(StoreContext);
-  };
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
 
 export { StoreProvider, useStoreContext };
