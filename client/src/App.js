82836487721dc0825479e469059f8287642d1e61
G Codes <graciela.mejia1@yahoo.com>
2022-12-04 19:11:56 -0800

    hompage.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
             <Route path="/success" element={<Success />} />
           </Switch>
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </Router>
   );
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,17 +1,35 @@
 import React from "react";
 
-// import Header from "../components/header";
-// import Footer from "../components/footer";
-// import Reviews from "../components/reviews";
-
 function HomePage() {
   return (
     <section>
-      {/* <Header /> */}
-      {/* <Reviews /> */}
-      <p>This is the homepage.</p>
-      {/* <Footer /> */}
+      <h1>
+        <center>Ready to break-up but don't know how?</center>
+      </h1>
+      <br />
+      <p>
+        <center>
+          Do you want the breakup to reflect your respect for your soon-to-be
+          ex? Drop a bundle on singers, dancers, flowers, gourmet treats, a spa
+          weekend.
+          <br />
+          <br />
+          Do you want the breakup to convey a sense of rage, betrayal, and
+          disdain? They can do that too.
+          <br />
+          <br />
+          Do you just want to take a break without closing the door forever?
+          Only professionals can say, ‘Don’t call me, I’ll call you’ without
+          hurting anyone’s feelings.
+          <br />
+          <br />
+          Away can send a quick notification to their facebook, or stage a troup
+          professional choristers on their doorstep. It’s your call. Simply
+          place your order and go ‘Away’.
+        </center>
+      </p>
     </section>
   );
 }
+
 export default HomePage;
