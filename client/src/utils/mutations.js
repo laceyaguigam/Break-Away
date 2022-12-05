298eda91cda882252c00c600e30f0d49034fa6dd
alexuschristensen <alexusdc6@gmail.com>
2022-12-04 16:40:54 -0800

    added jumbotron and add_order for mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,6 +12,24 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const ADD_ORDER = gql`
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
+        _id
+        name
+        description
+        price
+        quantity
+        category {
+          name
+        }
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
