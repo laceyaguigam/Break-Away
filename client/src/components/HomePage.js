e735ff74aae31c98e1abd77ef5082d4d3a8258db
G Codes <graciela.mejia1@yahoo.com>
2022-12-04 21:03:02 -0800

    update breakupartist/pricing/profilepage/css

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,31 +3,50 @@ import React from "react";
 function HomePage() {
   return (
     <section>
-      <h1>
-        <center>Ready to break-up but don't know how?</center>
-      </h1>
+      <div class="flex-row">
+        <h1>Ready to break-up but don't know how?</h1>
+      </div>
       <br />
-      <p>
-        <center>
-          Do you want the breakup to reflect your respect for your soon-to-be
-          ex? Drop a bundle on singers, dancers, flowers, gourmet treats, a spa
-          weekend.
-          <br />
-          <br />
-          Do you want the breakup to convey a sense of rage, betrayal, and
-          disdain? They can do that too.
-          <br />
-          <br />
-          Do you just want to take a break without closing the door forever?
-          Only professionals can say, ‘Don’t call me, I’ll call you’ without
-          hurting anyone’s feelings.
-          <br />
-          <br />
-          Away can send a quick notification to their facebook, or stage a troup
-          professional choristers on their doorstep. It’s your call. Simply
-          place your order and go ‘Away’.
-        </center>
-      </p>
+      <div class="row-home">
+        <div class="column-home">
+          <p>
+            Do you want the breakup to reflect your respect for your soon-to-be
+            ex?
+            <br />
+            <br />
+            Drop a bundle on singers, dancers, flowers, gourmet treats, a spa
+            weekend
+          </p>
+        </div>
+
+        <div class="column-home">
+          <p>
+            Do you want the breakup to convey a sense of rage, betrayal, and
+            disdain?
+            <br />
+            <br />
+            They can do that too.
+          </p>
+        </div>
+
+        <div class="column-home">
+          <p>
+            Do you just want to take a break without closing the door forever?
+            <br />
+            <br />
+            Only professionals can say, ‘Don’t call me, I’ll call you’ without
+            hurting anyone’s feelings.
+          </p>
+        </div>
+
+        <div class="column-home">
+          <p>
+            Away can send a quick notification to their facebook, or stage a
+            troup professional choristers on their doorstep. It’s your call.
+            Simply place your order and go ‘Away’.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,17 +1,17 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
-
 import Logo from "../assets/images/breakaway-logo.png";
 
 function Header() {
   return (
     <header className="fixed-header">
       <img src={Logo} height={100} alt="Break Away" />
-
       <ul>
         <li>
           <Link to="homepage">Home</Link>
+        </li>
+        <li>
+          <Link to="profilepage">Profile Page</Link>
         </li>
         <li>
           <Link to="breakupartists">Breakup Artists</Link>
@@ -19,10 +19,13 @@ function Header() {
         <li>
           <Link to="pricing">Pricing</Link>
         </li>
-        <li>
-          <Link to="reviews">Reviews</Link>
-        </li>
       </ul>
+      <div class="buttons">
+        <a href="./login.html">
+          <strong>Sign up</strong>
+        </a>
+        <a href="./login.html"> Log in</a>
+      </div>
       {/* <h1 className="fixed-site-title">BreakAway</h1> */}
     </header>
   );
