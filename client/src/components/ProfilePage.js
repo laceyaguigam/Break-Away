d3b49832c2fba564a810c8306902be6000192d46
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-04 20:37:35 -0800

    added second section of starter code to profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -17,6 +17,19 @@ function ProfilePage() {
         <Col>3 of 3</Col>
       </Row>
     </Container>
+
+{/* <Container>
+<Row>
+  <Col>1 of 2</Col>
+  <Col>2 of 2</Col>
+</Row>
+<Row>
+  <Col>1 of 3</Col>
+  <Col>2 of 3</Col>
+  <Col>3 of 3</Col>
+</Row>
+</Container> */}
+
   );
 
 }
