fbfc40c0c243674230eacdf37d3db2d6b2dcede4
G Codes <graciela.mejia1@yahoo.com>
2022-12-04 22:44:01 -0800

    updated css for homepage & pricing/import icons

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import BurningPicture from "../assets/images/burningpicture.jpg";
+import BurnBaby from "../assets/images/burnbaby.jpg";
+import BrokenHeart from "../assets/images/brokenheart.jpg";
+import RockPic from "../assets/images/aloneonrock.jpg";
+
 function HomePage() {
   return (
     <section>
@@ -9,20 +14,23 @@ function HomePage() {
       <br />
       <div class="row-home">
         <div class="column-home">
+          <img
+            src={BurningPicture}
+            alt="placeholder text"
+            style={{ width: "75%" }}
+          />
           <p>
-            Do you want the breakup to reflect your respect for your soon-to-be
-            ex?
+            Do you want the breakup to be respestful?
             <br />
             <br />
-            Drop a bundle on singers, dancers, flowers, gourmet treats, a spa
-            weekend
+            We got you!
           </p>
         </div>
 
         <div class="column-home">
+          <img src={BurnBaby} alt="placeholder text" style={{ width: "75%" }} />
           <p>
-            Do you want the breakup to convey a sense of rage, betrayal, and
-            disdain?
+            Want to have a sense of rage, betrayal and/or disdain?
             <br />
             <br />
             They can do that too.
@@ -30,20 +38,26 @@ function HomePage() {
         </div>
 
         <div class="column-home">
+          <img
+            src={BrokenHeart}
+            alt="placeholder text"
+            style={{ width: "75%" }}
+          />
           <p>
             Do you just want to take a break without closing the door forever?
             <br />
             <br />
-            Only professionals can say, ‘Don’t call me, I’ll call you’ without
-            hurting anyone’s feelings.
+            Ding, ding we can do that too!
           </p>
         </div>
 
         <div class="column-home">
+          <img src={RockPic} alt="placeholder text" style={{ width: "75%" }} />
           <p>
-            Away can send a quick notification to their facebook, or stage a
-            troup professional choristers on their doorstep. It’s your call.
-            Simply place your order and go ‘Away’.
+            How ever you do want to do it, we're here for you!
+            <br />
+            <br />
+            Checkout our pricing page.
           </p>
         </div>
       </div>
