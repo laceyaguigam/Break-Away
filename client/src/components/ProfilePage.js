98f35d323a61c1cebbe4b342b10fedd70407d53b
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-05 20:19:38 -0800

    added code for split screen in profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -9,7 +9,27 @@ import img7 from "../assets/images/breakaway-logo.png";
 
 function ProfilePage() {
   return (
-    
+   <div>
+  <div className="split left">
+    <div className="centered">
+      <img src="img_avatar2.png" alt="Avatar woman" />
+      <h2>Jane Flex</h2>
+      <p>Some text.</p>
+    </div>
+  </div>
+  <div className="split right">
+    <div className="centered">
+      
+   
+
+    </div>
+  </div>
+</div>
+
+
   );
 }
 export default ProfilePage;
+
+
+
