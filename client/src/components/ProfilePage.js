89d70796d976e3686e7c0ee27a86d032012547ac
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-05 19:48:11 -0800

    deleted previous code

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+
 
 import img1 from "../assets/images/alex.jpg";
 import img3 from "../assets/images/cassie.jpg";
@@ -13,68 +9,7 @@ import img7 from "../assets/images/breakaway-logo.png";
 
 function ProfilePage() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <img src={img7} alt="break away logo" class="logo" />
-          will have logo center top, then nav bar{" "}
-        </Col>
-        <Col>
-          My profile (center) under profile pic in circle then line under then
-          name location,
-        </Col>
-      </Row>
-      <Row>
-        <Col>cart</Col>
-        <Col>
-          break up artist
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    
   );
 }
 export default ProfilePage;
