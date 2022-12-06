7d2f31007d472d64d7a18c708709f7693666549c
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-05 20:26:14 -0800

    added breakup artist code to profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -20,7 +20,68 @@ function ProfilePage() {
   <div className="split right">
     <div className="centered">
       
-   
+    <section>
+     
+      <div class="row-breakup">
+        <div class="column-breakup">
+          <img
+            src={img1}
+            alt="photograph of Sarah the breakup artist"
+            style={{ width: "75%" }}
+          />
+          <h1>Alex</h1>
+          <p class="title">(insert info)</p>
+          <p>(insert info)</p>
+          <p>
+            <button>Hire</button>
+          </p>
+        </div>
+
+        <div class="column-breakup">
+          <img
+            src={img3}
+            alt="photograph of Sarah the breakup artist"
+            style={{ width: "60%" }}
+          />
+          <h1>Cassie</h1>
+          <p class="title">(insert info)</p>
+          <p>(insert info)</p>
+          <p>
+            <button>Hire</button>
+          </p>
+        </div>
+
+        <div class="column-breakup">
+          <img
+            src={img5}
+            alt="photograph of Sarah the breakup artist"
+            style={{ width: "60%" }}
+          />
+          <h1>Kyle</h1>
+          <p class="title">(insert info)</p>
+          <p>(insert info)</p>
+          <p>
+            <button>Hire</button>
+          </p>
+        </div>
+
+        <div class="column-breakup">
+          <img
+            src={img6}
+            alt="photograph of Sarah the breakup artist"
+            style={{ width: "60%" }}
+          />
+          <h1>Sarah</h1>
+          <p class="title">(insert info)</p>
+          <p>(insert info)</p>
+          <p>
+            <button>Hire</button>
+          </p>
+        </div>
+      </div>
+
+    </section>
+
 
     </div>
   </div>
