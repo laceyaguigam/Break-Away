c08159061d41bcf13ad13af3f115b90d6cb290be
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-12-06 06:55:55 -0800

    removed hire btns, updated blurbs on BreakupArtists

--- client/src/components/BreakupArtists.js
+++ client/src/components/BreakupArtists.js
@@ -6,7 +6,7 @@ import img3 from "../assets/images/cassie.jpg";
 // import img4 from "../assets/images/joe.jpg";
 import img5 from "../assets/images/kyle.jpg";
 import img6 from "../assets/images/sarah.jpg";
-
+ 
 function BreakupArtists() {
   return (
     <div>
@@ -15,15 +15,12 @@ function BreakupArtists() {
         <div class="column-breakup">
           <img
             src={img1}
-            alt="photograph of Sarah the breakup artist"
+            alt="Alex the breakup artist"
             style={{ width: "75%" }}
           />
           <h3>Alex</h3>
-          <p class="title">(insert info)</p>
-          <p>(insert info)</p>
-          <p>
-            <button>Hire</button>
-          </p>
+          <p class="title">Why Alex?</p>
+          <p>Describe specialties, qualifications, personality</p>
         </div>
 
         {/* <div class="column-breakup">
@@ -43,15 +40,12 @@ function BreakupArtists() {
         <div class="column-breakup">
           <img
             src={img3}
-            alt="photograph of Sarah the breakup artist"
+            alt="Cassie the breakup artist"
             style={{ width: "60%" }}
           />
           <h3>Cassie</h3>
-          <p class="title">(insert info)</p>
-          <p>(insert info)</p>
-          <p>
-            <button>Hire</button>
-          </p>
+          <p class="title">Why Cassie?</p>
+          <p>Describe specialties, qualifications, personality</p>
         </div>
 
         {/* <div class="column-breakup">
@@ -71,29 +65,23 @@ function BreakupArtists() {
         <div class="column-breakup">
           <img
             src={img5}
-            alt="photograph of Sarah the breakup artist"
+            alt="Kyle the breakup artist"
             style={{ width: "60%" }}
           />
           <h3>Kyle</h3>
-          <p class="title">(insert info)</p>
-          <p>(insert info)</p>
-          <p>
-            <button>Hire</button>
-          </p>
+          <p class="title">Why Kyle?</p>
+          <p>Describe specialties, qualifications, personality</p>
         </div>
 
         <div class="column-breakup">
           <img
             src={img6}
-            alt="photograph of Sarah the breakup artist"
+            alt="Sarah the breakup artist"
             style={{ width: "60%" }}
           />
           <h3>Sarah</h3>
-          <p class="title">(insert info)</p>
-          <p>(insert info)</p>
-          <p>
-            <button>Hire</button>
-          </p>
+          <p class="title">Why Sarah?</p>
+          <p>Describe specialties, qualifications, personality</p>
         </div>
       </div>
     </div>
