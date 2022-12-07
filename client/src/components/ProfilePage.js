c35fb9898048276a641637c5d845b7777bcbb0f9
lacey aguigam <laceyaguigam@yahoo.com>
2022-12-06 20:55:10 -0800

    imported stars into profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -6,6 +6,9 @@ import img5 from "../assets/images/kyle.jpg";
 import img6 from "../assets/images/sarah.jpg";
 import img7 from "../assets/images/liz.jpg";
 
+import {ImStarEmpty, ImStarFull} from "react-icons/im"
+
+
 function ProfilePage() {
   return (
     <div>
