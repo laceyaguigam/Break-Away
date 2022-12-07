6f400c60fda6091dda35d1c884a8ae0ea2ef8dba
alexuschristensen <alexusdc6@gmail.com>
2022-12-06 18:13:02 -0800

    updated app.js after accidental deletion

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 // import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Header from "./components/header";
 // import Footer from "./components/footer";
 import HomePage from "./components/HomePage";
@@ -12,6 +20,25 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   // const [token, setToken] = useState();
