dc67c81e7a9a5f02f92425cd0b26ceb8a6869f66
G Codes <graciela.mejia1@yahoo.com>
2022-12-07 21:09:20 -0800

    update CSS for profile page and login/signup buttons

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -6,13 +6,13 @@ import img5 from "../assets/images/kyle.jpg";
 import img6 from "../assets/images/sarah.jpg";
 import img7 from "../assets/images/liz.jpg";
 
-import {ImStarEmpty, ImStarFull} from "react-icons/im"
-import {FaBirthdayCake} from "react-icons/fa"
-import {GrMapLocation} from "react-icons/gr"
-import {SlEnvolope} from "react-icons/sl"
-import {AiOutlineCalendar} from "react-icons/ai"
-import {MdAttachMoney} from "react-icons/md"
-import {MdRateReview} from "react-icons/md"
+import { ImStarEmpty, ImStarFull } from "react-icons/im";
+import { FaBirthdayCake } from "react-icons/fa";
+import { GrMapLocation } from "react-icons/gr";
+import { SlEnvolope } from "react-icons/sl";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { MdAttachMoney } from "react-icons/md";
+import { MdRateReview } from "react-icons/md";
 
 function ProfilePage() {
   return (
@@ -20,139 +20,175 @@ function ProfilePage() {
       <div className="split left">
         <div className="centered">
           <section>
-          <h1 class="welcome">Welcome Liz!</h1>
-      </section>
+            <h1 className="welcome">Welcome Liz!</h1>
+          </section>
 
-        
           <img src={img7} alt="of a woman named Liz" />
-          
+
           <h2>Liz Smith</h2>
           <br></br>
-          <p><FaBirthdayCake/> 7.4.90</p>
-          <p><GrMapLocation/>Sacramento, CA</p>
-        
-      <br></br>
-     <br></br>
+          <p>
+            <FaBirthdayCake /> 7.4.90
+          </p>
+          <p>
+            <GrMapLocation />
+            Sacramento, CA
+          </p>
 
-      <table>
- 
- <tr>
-   <td><SlEnvolope/><a href="url" class="a">Messages</a></td>
-   <td><AiOutlineCalendar/><a href="url"class="a">Calendar</a></td>
- </tr>
- <tr>
-   <td><MdRateReview/><a href="url"class="a">Testimonials</a></td>
-   <td><MdAttachMoney/><a href="url"class="a">Become an Artist</a></td>
- </tr>
-</table> 
-   </div>   
-</div>
+          <br></br>
+          <br></br>
+
+          <table>
+            <tr>
+              <td>
+                <SlEnvolope />
+                <a href="url" class="a">
+                  Messages
+                </a>
+              </td>
+              <td>
+                <AiOutlineCalendar />
+                <a href="url" class="a">
+                  Calendar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <MdRateReview />
+                <a href="url" class="a">
+                  Testimonials
+                </a>
+              </td>
+              <td>
+                <MdAttachMoney />
+                <a href="url" class="a">
+                  Become an Artist
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
 
       <div className="split right">
         <div className="centered">
-    
-        <p class="centered1">Please Select your Artist </p>
+          <p class="righttitle">Please Select your Artist </p>
 
-          
-            <div class="row-breakup2">
-              <div class="column-breakup2">
-                <img
-                  src={img1}
-                  alt="photograph of Sarah the breakup artist"
-                  style={{ width: "75%" }}
-                />
+          <div class="row-breakup2">
+            <div class="column-breakup2">
+              <img
+                src={img1}
+                alt="photograph of Sarah the breakup artist"
+                style={{ width: "75%" }}
+              />
 
-                <h1>Alex</h1>
-                <p class="title">Rating: 
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarEmpty/>
-          <ImStarEmpty/></p>
-          <dl>
-          <p class="p1"><dt>Specialties: </dt></p>
-              <dd> -Phone Call</dd>
-                <dd>-FB Message </dd>
-                  <dd> -Flowers</dd>
-                  </dl>
-                  <p>
-                  <button>Hire</button>
-                  </p>
-              </div>
-
-              <div class="column-breakup2">
-                <img
-                  src={img3}
-                  alt="photograph of Sarah the breakup artist"
-                  style={{ width: "60%" }}
-                />
-                <h1>Cassie</h1>
-                <p class="title">Rating: 
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarEmpty/></p>
-          <dl>
-          <p class="p1"><dt>Specialties: </dt></p>
-              <dd> -Acapella</dd>
-                <dd>-Hand-written letter </dd>
-                  <dd> -Playlist</dd>
-                    </dl>
-                    <p>
-                  <button>Hire</button>
-                  </p>
-              </div>
-
-              <div class="column-breakup2">
-                <img
-                  src={img5}
-                  alt="photograph of Sarah the breakup artist"
-                  style={{ width: "60%" }}
-                />
-                <h1>Kyle</h1>
-                <p class="title">Rating: 
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/></p>
-          <dl>
-          <p class="p1"><dt>Specialties: </dt></p>
-              <dd> -Acapella</dd>
-                <dd>-Chocolate </dd>
-                  <dd> -Email</dd>
-                  </dl>
-                <p>
-                  <button>Hire</button>
-                  </p>
-              </div>
-
-              <div class="column-breakup2">
-                <img
-                  src={img6}
-                  alt="photograph of Sarah the breakup artist"
-                  style={{ width: "60%" }}
-                />
-                <h1>Sarah</h1>
-                <p class="title">Rating: 
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarFull/>
-          <ImStarEmpty/></p>
-          <dl>
-          <p class="p1"><dt>Specialties: </dt></p>
-              <dd> -Flowers</dd>
-                <dd>-Hand-written letter </dd>
-                  <dd> -Playlist</dd>
-                    </dl>
-                <p>
-                  <button>Hire</button>
+              <h1>Alex</h1>
+              <p class="title">
+                Rating:
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarEmpty />
+                <ImStarEmpty />
+              </p>
+              <dl>
+                <p class="p1">
+                  <dt>Specialties: </dt>
                 </p>
-              </div>
+                <dd> -Phone Call</dd>
+                <dd>-FB Message </dd>
+                <dd> -Flowers</dd>
+              </dl>
+              <p>
+                <button>Hire</button>
+              </p>
             </div>
-          
+
+            <div class="column-breakup2">
+              <img
+                src={img3}
+                alt="photograph of Sarah the breakup artist"
+                style={{ width: "60%" }}
+              />
+              <h1>Cassie</h1>
+              <p class="title">
+                Rating:
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarEmpty />
+              </p>
+              <dl>
+                <p class="p1">
+                  <dt>Specialties: </dt>
+                </p>
+                <dd> -Acapella</dd>
+                <dd>-Hand-written letter </dd>
+                <dd> -Playlist</dd>
+              </dl>
+              <p>
+                <button>Hire</button>
+              </p>
+            </div>
+
+            <div class="column-breakup2">
+              <img
+                src={img5}
+                alt="photograph of Sarah the breakup artist"
+                style={{ width: "60%" }}
+              />
+              <h1>Kyle</h1>
+              <p class="title">
+                Rating:
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+              </p>
+              <dl>
+                <p class="p1">
+                  <dt>Specialties: </dt>
+                </p>
+                <dd> -Acapella</dd>
+                <dd>-Chocolate </dd>
+                <dd> -Email</dd>
+              </dl>
+              <p>
+                <button>Hire</button>
+              </p>
+            </div>
+
+            <div class="column-breakup2">
+              <img
+                src={img6}
+                alt="photograph of Sarah the breakup artist"
+                style={{ width: "60%" }}
+              />
+              <h1>Sarah</h1>
+              <p class="title">
+                Rating:
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarFull />
+                <ImStarEmpty />
+              </p>
+              <dl>
+                <p class="p1">
+                  <dt>Specialties: </dt>
+                </p>
+                <dd> -Flowers</dd>
+                <dd>-Hand-written letter </dd>
+                <dd> -Playlist</dd>
+              </dl>
+              <p>
+                <button>Hire</button>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -6,52 +6,51 @@ import Logo from "../assets/images/breakaway-logo.png";
 function Header() {
   return (
     <>
-    <div className="signup-login-container">
-    <ul className="signup-login-buttons">
-        <li className="mx-1">
-          <Link style={{ textDecoration: "none" }} to="signup">
-            Signup
-          </Link>
-        </li>
-        <li className="mx-1">
-          <Link style={{ textDecoration: "none" }} to="login">
-            Login
-          </Link>
-        </li>
-      </ul>
+      <div className="signup-login-container">
+        <ul className="signup-login-buttons">
+          <li>
+            <Link style={{ textDecoration: "none" }} to="signup">
+              Signup
+            </Link>
+          </li>
+          <li>
+            <Link style={{ textDecoration: "none" }} to="login">
+              Login
+            </Link>
+          </li>
+        </ul>
       </div>
-    <header className="fixed-header">
-        <div style={{width: "100%"}}>
-      <div className="logo-navbar">
-      <img src={Logo} height={130} alt="Break Away" />
-      {/* <br></br> */}
-      <ul>
-        <li>
-          <Link style={{ textDecoration: "none" }} to="homepage">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none" }} to="profilepage">
-            Profile Page
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none" }} to="breakupartists">
-            Breakup Artists
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none" }} to="pricing">
-            Pricing
-          </Link>
-        </li>
-      </ul>
-      </div>
-      </div>
-    </header>
-      
-      </>
+      <header className="fixed-header">
+        <div style={{ width: "100%" }}>
+          <div className="logo-navbar">
+            <img src={Logo} height={130} alt="Break Away" />
+            {/* <br></br> */}
+            <ul>
+              <li>
+                <Link style={{ textDecoration: "none" }} to="homepage">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link style={{ textDecoration: "none" }} to="profilepage">
+                  Profile Page
+                </Link>
+              </li>
+              <li>
+                <Link style={{ textDecoration: "none" }} to="breakupartists">
+                  Breakup Artists
+                </Link>
+              </li>
+              <li>
+                <Link style={{ textDecoration: "none" }} to="pricing">
+                  Pricing
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
 
